0ca98359bd93c90e89948651a0bb35b82e1496b6
kitlawes <kitlawes@hotmail.com>
2018-04-13 18:07:02 +0100

    Show a visualisation of the days in the current month

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.*;
+import java.time.DayOfWeek;
 
 public class Calendar extends JFrame {
 
@@ -7,10 +9,43 @@ public class Calendar extends JFrame {
     }
 
     public Calendar() {
-        setSize(500, 500);
-        setLocation(100, 100);
+
+        setSize(100 * 7 + 17, 400 + 40);
+        setLocation(3100, 600);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setLayout(null);
+
+        JTextArea jTextArea = new JTextArea();
+        jTextArea.setText("APRIL");
+        jTextArea.setSize(100 * 7 + 1, 50 + 1);
+        jTextArea.setLocation(0, 0);
+        jTextArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        add(jTextArea);
+
+        for (int i = 0; i < 7; i++) {
+                jTextArea = new JTextArea();
+                jTextArea.setText(DayOfWeek.of(i + 1).toString());
+                jTextArea.setSize(100 + 1, 50 + 1);
+                jTextArea.setLocation(100 * i, 50);
+                jTextArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                add(jTextArea);
+        }
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 6; j++) {
+                jTextArea = new JTextArea();
+                int date = i + j * 7 - 5;
+                jTextArea.setText(date >= 1 && date <= 30 ? String.valueOf(date) : "");
+                jTextArea.setSize(100 + 1, 50 + 1);
+                jTextArea.setLocation(100 * i, 100 + 50 * j);
+                jTextArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                add(jTextArea);
+            }
+        }
+
         setVisible(true);
+
     }
 
 }
